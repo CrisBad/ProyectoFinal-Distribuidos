b2f8952c29896a87353ef6a3d69c6fb72d7eae7a
danielcis25 <dcisneros@unicauca.edu.co>
2023-02-04 23:02:22 -0500

    new commit

--- cliente_administrador/src/main/java/co/edu/unicauca/distribuidos/cliente_administrador/model/clienteAdmin.java
+++ cliente_administrador/src/main/java/co/edu/unicauca/distribuidos/cliente_administrador/model/clienteAdmin.java
@@ -1,5 +1,9 @@
 package co.edu.unicauca.distribuidos.cliente_administrador.model;
-
+import java.util.Date;
 public class clienteAdmin {
+    private String nombre;
+    private String apellido;
+    private String username;
+    private String password;
     
 }
