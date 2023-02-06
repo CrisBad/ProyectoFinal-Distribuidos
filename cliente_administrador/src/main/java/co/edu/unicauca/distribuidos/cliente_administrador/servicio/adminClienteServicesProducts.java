851f9317f4933cf059c28ea9f1b1c9712c4a163f
danielcis25 <dcisneros@unicauca.edu.co>
2023-02-05 20:32:55 -0500

    correccion error

--- cliente_administrador/src/main/java/co/edu/unicauca/distribuidos/cliente_administrador/servicio/adminClienteServicesProducts.java
+++ cliente_administrador/src/main/java/co/edu/unicauca/distribuidos/cliente_administrador/servicio/adminClienteServicesProducts.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-//import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
@@ -11,8 +11,8 @@ import javax.ws.rs.core.MediaType;
 //import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.jackson.JacksonFeature;
-//import org.springframework.http.HttpStatus;
-//import co.edu.unicauca.distribuidos.cliente_administrador.model.clienteAdmin;
+import org.springframework.http.HttpStatus;
+import co.edu.unicauca.distribuidos.cliente_administrador.model.clienteAdmin;
 import co.edu.unicauca.distribuidos.cliente_administrador.model.ProductoEntity;
 
 public class adminClienteServicesProducts {
